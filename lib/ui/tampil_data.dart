7ddf7d42b5374bed4fc4548ef0eb3d15b64a8384
DestianArdanAlfatanu <150760697+DestianArdanAlfatanu@users.noreply.github.com>
2024-09-18 23:41:04 +0700

    Update tampil_data.dart

--- lib/ui/tampil_data.dart
+++ lib/ui/tampil_data.dart
@@ -17,13 +17,24 @@ class TampilData extends StatelessWidget {
     final int umur = DateTime.now().year - tahun;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Perkenalan"),
+        title: const Text(
+          "Perkenalan",
+          style: TextStyle(color: Colors.white), // Mengubah warna teks menjadi putih
+        ),
+        backgroundColor: Colors.red, // Mengubah warna AppBar menjadi merah
       ),
       body: Container(
         margin: const EdgeInsets.all(10),
+        color: Colors.white, // Background tetap putih
         child: Column(
           children: [
-            Text("Nama saya $nama, NIM $nim, dan umur saya adalah $umur tahun"),
+            Text(
+              "Nama saya $nama, NIM $nim, dan umur saya adalah $umur tahun",
+              style: const TextStyle(
+                color: Colors.black, // Mengubah warna teks menjadi merah
+                fontSize: 24, // Ukuran teks dapat disesuaikan
+              ),
+            ),
           ],
         ),
       ),
